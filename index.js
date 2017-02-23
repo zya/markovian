14a0cd5712b6b75b84ebfea551903bbda425b047
Ehsan Ziya <ehsan.ziya@gmail.com>
2017-02-23 00:46:45 +0000

    Fix: Export the correct module

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 'use strict';
 
-var markov = require('./lib/markov');
+var markovian = require('./lib/markovian');
 
-var zero = {
-  value: 'zero',
-  targets: [0, 1, 2],
-  probabilities: [0.25, 0.25, 0.50]
-};
+// var zero = {
+//   value: 'zero',
+//   targets: [0, 1, 2],
+//   probabilities: [0.25, 0.25, 0.50]
+// };
+//
+// var one = {
+//   value: 'one',
+//   targets: [0, 2],
+//   probabilities: [0.25, 0.75]
+// };
+//
+// var two = {
+//   value: 'two',
+//   targets: [0, 1],
+//   probabilities: [0.80, 0.20]
+// };
+//
+// var states = [zero, one, two];
+// var model = markovian.create(states);
+//
+// setInterval(function () {
+//   var state = model.tick();
+//   console.log(state);
+// }, 500);
 
-var one = {
-  value: 'one',
-  targets: [0, 2],
-  probabilities: [0.25, 0.75]
-};
-
-var two = {
-  value: 'two',
-  targets: [0, 1],
-  probabilities: [0.80, 0.20]
-};
-
-var states = [zero, one, two];
-var model = markov.create(states);
-
-setInterval(function(){
-  var state = model.tick();
-  console.log(state);
-}, 500);
+module.exports = markovian;
